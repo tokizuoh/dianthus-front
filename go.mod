ba4fd5dbf009cd1985fbf101fadae5a76fbc38af
tokizuoh <k_sakai@yumemi.co.jp>
2021-07-03 16:48:26 +0900

    add env setting: basic-auth id, sec

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tokiozuoh/dianthus-front
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.3.0 // indirect
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/labstack/echo/v4 v4.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"text/template"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
@@ -51,12 +53,18 @@ func main() {
 
 func fetchWords() ([]Word, error) {
 	client := &http.Client{Timeout: time.Duration(30) * time.Second}
-	req, err := http.NewRequest("GET", "http://172.30.0.3:8080/v1/roman?target=amana", nil)
+	req, err := http.NewRequest("GET", "http://172.30.0.3:8080/v1/roman?target=chiyuki", nil)
 	if err != nil {
 		return nil, err
 	}
 
-	req.SetBasicAuth("taxislu22pkjzjel", "xwu8cl4ocfwh3c5w")
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
+
+	cliID := os.Getenv("BASIC_AUTH_CLIENT_ID")
+	cliSec := os.Getenv("BASIC_AUTH_CLIENT_SECRET")
+	req.SetBasicAuth(cliID, cliSec)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
